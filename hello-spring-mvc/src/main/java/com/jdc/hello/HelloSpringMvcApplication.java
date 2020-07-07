42c31598c93a71150b8e9ace0244b9442392444e
minlwin <lwin.zawmin@gmail.com>
2020-07-07 21:17:31 +0630

    Share Hello MVC

---
+++ hello-spring-mvc/src/main/java/com/jdc/hello/HelloSpringMvcApplication.java
@@ -0,0 +1,13 @@
+package com.jdc.hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloSpringMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloSpringMvcApplication.class, args);
+	}
+
+}
